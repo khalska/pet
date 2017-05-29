449514cc5ec2c5c2604a5b91b84ac5edd9536bb7
khalska <katarzynahalska@wp.pl>
2017-05-29 13:26:52 +0200

    Changes in routes.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import Main from "../../routes";
+import getRoutes from '../../routes';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
         <Header title="Welcome to React" />
-        <Main />
+        { getRoutes() }
         <Footer />
       </div>
     );
--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,12 @@ import { Router, Route, browserHistory } from 'react-router';
 import Page from "./components/Page/Page";
 import PostPage from "./components/PostPage/PostPage";
 
-const Main = () => (
-  <main>
-    <Router history={browserHistory}>
-      <Route exact path='/' component={Page}/>
-      <Route path='/add-post' component={PostPage}/>
-      <Route path='/update-post/:postId' component={PostPage}/>
-    </Router>
-  </main>
-)
-
-export default Main;
+export default () => {
+  return (
+      <Router history={browserHistory}>
+        <Route exact path='/' component={Page}/>
+        <Route path='/add-post' component={PostPage}/>
+        <Route path='/update-post/:postId' component={PostPage}/>
+      </Router>
+    );
+};
